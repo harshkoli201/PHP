6e84aa8f4c53c0a50cd9f72062c9f866ae41f42d
Harsh Koli <harshkoli201@gmail.com>
2021-04-17 12:45:26 +0530

    exp1

---
+++ bitwise.php
@@ -0,0 +1,30 @@
+<?php
+$a = 2;
+$b = 4;
+$ans = $a&$b;
+echo $ans;
+
+echo "<br>";
+$ans = $a|$b;
+echo $ans;
+
+echo "<br>";
+$ans = $a^$b;
+echo $ans;
+
+echo "<br>";
+$ans = ~$a;
+echo $ans;
+
+echo "<br>";
+$ans = $a<<$b;
+echo $ans;
+
+echo "<br>";
+$ans = $a>>$b;
+echo $ans;
+
+
+
+
+?>
