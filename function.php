91a0475bf042c0d3394a6dc27e4d26a55707288f
Harsh Koli <harshkoli201@gmail.com>
2021-04-23 11:41:35 +0530

    function

---
+++ function.php
@@ -0,0 +1,14 @@
+<?php
+function area1($c)
+{
+    return 3.14*$c*$c;
+}
+
+function rec1($l,$b)
+{
+    return $l*$b;
+}
+echo "Area of circle: ", area1(2);
+echo "<br> Area of rectangle: ", rec1(2,3);
+?>
+
